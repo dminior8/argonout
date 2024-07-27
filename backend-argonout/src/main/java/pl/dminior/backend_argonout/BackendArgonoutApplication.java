08909fc6746ca9ba6183ed4e6719ff368074153f
Daniel <daniel.minior.pk@gmail.com>
2024-07-27 23:53:06 +0200

    Backend init

---
+++ backend-argonout/src/main/java/pl/dminior/backend_argonout/BackendArgonoutApplication.java
@@ -0,0 +1,13 @@
+package pl.dminior.backend_argonout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArgonoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArgonoutApplication.class, args);
+	}
+
+}
---
+++ backend-argonout/src/test/java/pl/dminior/backend_argonout/BackendArgonoutApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dminior.backend_argonout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendArgonoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
