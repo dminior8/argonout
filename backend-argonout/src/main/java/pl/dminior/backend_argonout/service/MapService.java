e3a2451487ba05e56c0fa17525c0b93f8deab13e
Daniel <daniel.minior.pk@gmail.com>
2024-09-23 23:58:14 +0200

    Backend map impl

---
+++ backend-argonout/src/main/java/pl/dminior/backend_argonout/service/MapService.java
@@ -0,0 +1,9 @@
+package pl.dminior.backend_argonout.service;
+
+import pl.dminior.backend_argonout.model.Place;
+
+import java.util.List;
+
+public interface MapService {
+    List<Place> getAllLocations();
+}
